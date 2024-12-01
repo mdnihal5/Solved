fae26585ad7c105f83def6c52849ae3ff4e370a9
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-12-01 10:40:11 +0530

    Update RerootingDp.cpp

--- RerootingDp.cpp
+++ RerootingDp.cpp
@@ -3,63 +3,68 @@
 // solution : https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-ii/submissions/1467079675
 
 
-#include <iostream>
-#include <vector>
-using namespace std;
+#define ll long long
 
 class Solution {
-public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+  public:
+    vector<int> maxTargetNodes (vector<vector<int>> &edges1, vector<vector<int>> &edges2) {
         int n = edges1.size() + 1, m = edges2.size() + 1;
-        vector<vector<int>> g1(n), g2(m);
+        vector<vector<int>> g1 (n), g2 (m);
 
-        for (const auto& edge : edges1) {
-            g1[edge[0]].push_back(edge[1]);
-            g1[edge[1]].push_back(edge[0]);
-        }
-        for (const auto& edge : edges2) {
-            g2[edge[0]].push_back(edge[1]);
-            g2[edge[1]].push_back(edge[0]);
+        for (const auto &edge : edges1) {
+            g1[edge[0]].push_back (edge[1]);
+            g1[edge[1]].push_back (edge[0]);
         }
 
-        auto solve = [&](auto &graph) -> pair<vector<int>,vector<int>> {
-            
-            int n=graph.size();
-            vector<int> e1(n),e2(n),o1(n),o2(n);
-            
-            auto dfs1=[&](int node,int par,auto &&dfs)->void{
-                e1[node]=1;
-                for(auto &child:graph[node]){
-                    if(child==par) continue;
-                    dfs(child,node,dfs);
-                    e1[node]+=o1[child];
-                    o1[node]+=e1[child];
+        for (const auto &edge : edges2) {
+            g2[edge[0]].push_back (edge[1]);
+            g2[edge[1]].push_back (edge[0]);
+        }
+
+        auto solve = [&] (auto & graph) -> pair<vector<int>, vector<int>> {
+
+            int n = graph.size();
+            vector<int> e1 (n), e2 (n), o1 (n), o2 (n);
+            // calculate DP ( for a single node )
+            auto dfs1 = [&] (int node, int par, auto &&dfs)->void{
+                e1[node] = 1;
+
+                for (auto &child : graph[node]) {
+                    if (child == par) continue;
+
+                    dfs (child, node, dfs);
+                    e1[node] += o1[child];
+                    o1[node] += e1[child];
                 }
             };
-            
-            dfs1(0,0,dfs1);
-            
-            e2[0]=e1[0],o2[0]=o1[0];
-            auto dfs2=[&](int node,int par,auto &&dfs)->void{
-                  for(auto &child:graph[node]){
-                      if(child==par) continue;
-                      o2[child]=(e2[node]-o1[child])+o1[child];
-                      e2[child]=(o2[node]-e1[child])+e1[child];
-                      dfs(child,node,dfs);
-                  }
+
+            dfs1 (0, 0, dfs1);
+            // apply Rerooting DP  ( for all nodes )
+            e2[0] = e1[0], o2[0] = o1[0];
+            auto dfs2 = [&] (int node, int par, auto &&dfs)->void{
+                for (auto &child : graph[node]) {
+                    if (child == par) continue;
+
+                    o2[child] = (e2[node] - o1[child]) + o1[child];
+                    e2[child] = (o2[node] - e1[child]) + e1[child];
+                    dfs (child, node, dfs);
+                }
             };
-            dfs2(0,0,dfs2);
-            
-            return {e2,o2};
+            dfs2 (0, 0, dfs2);
+
+            return {e2, o2};
         };
 
-        auto [x1, y1] = solve(g2);
-        auto [x2,y2]=solve(g1);
+        auto [x1, y1] = solve (g2);
+        auto [x2, y2] = solve (g1);
         vector<int>ans;
-        int mx=*max_element(y1.begin(),y1.end());
-        for(int i=0;i<n;i++){
-            ans.push_back(x2[i]+mx);
+        int mx = *max_element (y1.begin(), y1.end() );
+
+        for (int i = 0; i < n; i++) {
+            ans.push_back (x2[i] + mx);
         }
+
         return ans;
     }
 };
+
